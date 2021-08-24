734568e9f7072bc0f9528745388cf9582405a252
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-24 21:15:32 +0530

    Added:Wages for a Month

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -6,13 +6,13 @@ public class EmpWageBuilder {
 		 public static final int IS_FULL_TIME=2;
 		 public static final int EMP_RATE_PER_HOUR=20;
 		 public static final int IS_PART_TIME=1;
+		 public static final int NUM_OF_WORKING_DAYS=20;
 		 public static void main(String[] args) {
 		 //Variables
-		 int empHrs=0;
-		 int empWage=0;
+		 int empHrs=0 ,empWage=0 ,totalEmpWage=0;
 		 //Computation
-		 
-		 double empCheck = Math.floor(Math.random()*10)%3;
+		 for(int day=0; day<NUM_OF_WORKING_DAYS;day++) {
+		 int empCheck =(int) Math.floor(Math.random()*10)%3;
 		 switch((int)empCheck) {
 		 
 		 case IS_FULL_TIME:
@@ -25,8 +25,12 @@ public class EmpWageBuilder {
 		   empHrs=0;
 		 }
 		 empWage=empHrs*EMP_RATE_PER_HOUR;
+		 totalEmpWage+=empWage;
 		 System.out.println("EmpWage"+empWage);
+		 System.out.println("TotalEmpWage"+totalEmpWage);
+		 
 					 
   }
 
+}
 }
