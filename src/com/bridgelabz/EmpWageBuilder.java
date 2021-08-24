86a283000b69c9b71350a0932b18504b93cf7db3
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-24 13:03:40 +0530

    Added:Checking Employee Attendance

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -3,6 +3,13 @@ package com.bridgelabz;
 public class EmpWageBuilder {
 	public static void main(String[] args) {
 		 System.out.print("Welcome to Employee Wage Computation Program");
-	}
+		 int IS_FULL_TIME=1;
+		 double empCheck = Math.floor(Math.random()*10)%2;	
+				 if(empCheck==IS_FULL_TIME)
+					 System.out.println("\nEmployee is Present");
+				 else
+					 System.out.println("\nEmployee is Absent");
+					 
+  }
 
 }
