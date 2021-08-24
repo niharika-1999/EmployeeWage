42b5f86e1114b0313a2023f8d14e837004689de1
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-24 21:37:02 +0530

    Added:ConditionWages

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -7,30 +7,33 @@ public class EmpWageBuilder {
 		 public static final int EMP_RATE_PER_HOUR=20;
 		 public static final int IS_PART_TIME=1;
 		 public static final int NUM_OF_WORKING_DAYS=20;
+		 public static final int MAX_HRS_IN_MONTH=10;
 		 public static void main(String[] args) {
 		 //Variables
-		 int empHrs=0 ,empWage=0 ,totalEmpWage=0;
+		 int empHrs=0 ,empWage=0 ,totalEmpWage=0,totalWorkingDays=0,totalEmpHrs=0;
 		 //Computation
-		 for(int day=0; day<NUM_OF_WORKING_DAYS;day++) {
-		 int empCheck =(int) Math.floor(Math.random()*10)%3;
-		 switch((int)empCheck) {
+		 while(totalEmpHrs<=MAX_HRS_IN_MONTH&&totalWorkingDays<NUM_OF_WORKING_DAYS) {
+			 totalWorkingDays++;
+			 int empCheck =(int) Math.floor(Math.random()*10)%3;
+		     switch((int)empCheck) {
 		 
-		 case IS_FULL_TIME:
-		   empHrs=8;
-		   break;
-		 case IS_PART_TIME:
-		   empHrs=4;
-		   break;
-		 default:
-		   empHrs=0;
-		 }
-		 empWage=empHrs*EMP_RATE_PER_HOUR;
-		 totalEmpWage+=empWage;
-		 System.out.println("EmpWage"+empWage);
-		 System.out.println("TotalEmpWage"+totalEmpWage);
-		 
+		     case IS_FULL_TIME:
+			   empHrs=8;
+			   break;
+			 case IS_PART_TIME:
+			   empHrs=4;
+			   break;
+			 default:
+			   empHrs=0;
+			 }
+		     totalEmpHrs+=empHrs;
+			 empWage=empHrs*EMP_RATE_PER_HOUR;
+			 totalEmpWage+=empWage;
+			 System.out.println("EmpWage"+empWage);
+			 System.out.println("TotalEmpWage"+totalEmpWage);
+			 
 					 
-  }
+   }
 
-}
+  }
 }
