6f2abdc32f673e99ecafb63211391c0d9b052465
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-25 20:44:38 +0530

    Added:Refactor Code to compute Employee Wage

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -4,36 +4,48 @@ public class EmpWageBuilder {
 	
 		 //Constants
 		 public static final int IS_FULL_TIME=2;
-		 public static final int EMP_RATE_PER_HOUR=20;
 		 public static final int IS_PART_TIME=1;
-		 public static final int NUM_OF_WORKING_DAYS=20;
-		 public static final int MAX_HRS_IN_MONTH=10;
+		 private final String Company;
+		 private final int EmpRatePerHour;
+		 private final int NumOfWorkingDays;
+		 private final int MaxHoursPerMonth;
+		 
+		 public EmpWageBuilder(String Company,int EmpRatePerHour, int NumOfWorkingDays,int MaxHoursPerMonth)
+		 {
+			 this.Company=Company;
+			 this.EmpRatePerHour=EmpRatePerHour;
+			 this.NumOfWorkingDays=NumOfWorkingDays;
+			 this.MaxHoursPerMonth=MaxHoursPerMonth;
+		 }
+		 
 		 public static void main(String[] args) {
+			 EmpWageBuilder DMart=new EmpWageBuilder("DMart",20,2,10); 
 		 //Variables
-		 int empHrs=0 ,empWage=0 ,totalEmpWage=0,totalWorkingDays=0,totalEmpHrs=0;
+		 int EmpHrs=0,totalWorkingDays=0,totalEmpHrs=0;
 		 //Computation
-		 while(totalEmpHrs<=MAX_HRS_IN_MONTH&&totalWorkingDays<NUM_OF_WORKING_DAYS) {
+		
+		 while(totalEmpHrs<=DMart.MaxHoursPerMonth && totalWorkingDays<=DMart.NumOfWorkingDays) {
 			 totalWorkingDays++;
-			 int empCheck =(int) Math.floor(Math.random()*10)%3;
-		     switch((int)empCheck) {
+			 int EmpCheck =(int) Math.floor(Math.random()*10)%3;
+		     switch(EmpCheck) {
 		 
 		     case IS_FULL_TIME:
-			   empHrs=8;
+			   EmpHrs=8;
 			   break;
 			 case IS_PART_TIME:
-			   empHrs=4;
+			   EmpHrs=4;
 			   break;
 			 default:
-			   empHrs=0;
+			   EmpHrs=0;
 			 }
-		     totalEmpHrs+=empHrs;
-			 empWage=empHrs*EMP_RATE_PER_HOUR;
-			 totalEmpWage+=empWage;
-			 System.out.println("EmpWage"+empWage);
-			 System.out.println("TotalEmpWage"+totalEmpWage);
+		     totalEmpHrs+=EmpHrs;
+             System.out.println("Day#:"+totalWorkingDays+ "EmpHrs:"+EmpHrs);
+		 }
+             int totalEmpWage=totalEmpHrs*DMart.EmpRatePerHour;
+			 System.out.println("TotalEmpWage for Company:"+DMart.Company+ " is:"+totalEmpWage);
 			 
 					 
    }
 
   }
-}
+
